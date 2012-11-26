4582a26fd56407b277da0b36d5578071ffa46055
Burton Betchart <betchart@gmail.com>
2012-11-26 22:46:39 +0000

    more LV cpp

--- cpp/linkdef.cxx
+++ cpp/linkdef.cxx
@@ -4,6 +4,9 @@
 #include "Math/RotationZ.h"
 #ifdef __CINT__
 typedef ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiM4D<double> > LV;
+#pragma link C++ function ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiM4D<double> >::operator+(LV);
+#pragma link C++ function ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiM4D<double> >::operator-(LV);
+#pragma link C++ function ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiM4D<double> >::Dot(LV);
 #pragma link C++ function ROOT::Math::BoostZ::operator()(LV);
 #pragma link C++ function ROOT::Math::Boost::operator()(LV);
 #pragma link C++ function ROOT::Math::RotationZ::operator()(LV);
