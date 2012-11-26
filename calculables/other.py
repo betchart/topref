4a9554ba0423ba212bc7764a274f900fea1c5877
Burton Betchart <betchart@gmail.com>
2012-11-26 20:52:40 +0000

    calculables met, overhaul

--- calculables/other.py
+++ calculables/other.py
@@ -11,34 +11,6 @@ class pthatLess(wrappedChain.calculable) :
         self.maxPtHat = maxPtHat
     def update(self,ignored) : self.value = None if self.maxPtHat < self.source["genpthat"] else 1
 ##############################
-class Mt(wrappedChain.calculable) :
-    @property
-    def name(self) :
-        return "%sMt%s%s"%(self.fixes[0], self.fixes[1], self.met)
-    
-    def __init__(self, collection = None, met = None, byHand = True , allowNonIso = False, isSumP4=False) :
-        self.met = met
-        self.fixes = collection
-        self.stash(["Indices","IndicesNonIso","P4"])
-        self.byHand = byHand
-        self.isSumP4 = isSumP4
-        self.allowNonIso = allowNonIso
-        self.moreName = "%s%s, %s, byHand=%d"%(collection[0], collection[1], met, byHand)
-
-    def update(self, ignored) :
-        if (not self.source[self.Indices]) and not (self.allowNonIso or self.source[self.indicesNonIso]) :
-            self.value= -1.0
-            return
-        index = self.source[self.Indices][0] if self.source[self.Indices] else \
-                self.source[self.IndicesNonIso][0]
-        lep = self.source[self.P4][index]
-        met = self.source[self.met] * (-1 if self.isSumP4 else 1)
-
-        if self.byHand :
-            self.value = math.sqrt( 2.0*lep.pt()*met.pt()*(1.0 - math.cos(r.Math.VectorUtil.DeltaPhi(lep, met))) )
-        else :
-            self.value = (lep+met).Mt()
-#####################################
 class jsonWeight(wrappedChain.calculable) :
     def __init__(self, fileName = "", acceptFutureRuns = False) :
         self.moreName = "run:ls in %s"%fileName
@@ -79,14 +51,6 @@ class PtSorted(wrappedChain.calculable) :
     def update(self,_) :
         pt = self.source[self.Pt]
         self.value = all(i>j for i,j in zip(pt[:-1],pt[1:]))
-#####################################
-class Covariance(wrappedChain.calculable) :
-    def __init__(self, collection) :
-        self.fixes = collection
-        self.stash(['SigmaXX','SigmaXY','SigmaYY'])
-    def update(self,_) :
-        self.value = np.array([[self.source[self.SigmaXX],self.source[self.SigmaXY]],
-                               [self.source[self.SigmaXY],self.source[self.SigmaYY]]])
 #####################################
 class KarlsruheDiscriminant(wrappedChain.calculable) :
     def __init__(self, jet, met) :
