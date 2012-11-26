49dc01769e1f488d73a6484ca028f98ca3fd6aba
Burton Betchart <betchart@gmail.com>
2012-11-26 17:13:40 +0000

    vector utilities cpp/

---
+++ cpp/linkdef.cxx
@@ -0,0 +1,10 @@
+#include "Math/LorentzVector.h"
+#include "Math/VectorUtil.h"
+#include "Math/BoostZ.h"
+#include "Math/RotationZ.h"
+#ifdef __CINT__
+typedef ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiM4D<double> > LV;
+#pragma link C++ function ROOT::Math::BoostZ::operator()(LV);
+#pragma link C++ function ROOT::Math::Boost::operator()(LV);
+#pragma link C++ function ROOT::Math::RotationZ::operator()(LV);
+#endif
