59122b968183b440835d1a62dd25c4c7d6104dbf
Burton Betchart <betchart@gmail.com>
2013-01-21 20:01:59 +0000

    more v118 updates

--- samples/__lepton118__.py
+++ samples/__lepton118__.py
@@ -15,7 +15,7 @@ lepton118.add("MuHad.2012A_2", '%s/%s")'%(srm_burt, loc%"Mu"),       lumi = 9999
 
 loc = "2013_01_15_22_26_19/Single%s.Run2012B-13Jul2012-v1.AOD"
 lepton118.add("SingleEl.2012B", '%s/%s")'%(srm_burt, loc%'Electron'), lumi = 9999 ) #4408.9410 )
-lepton118.add("SingleMu.2012B", '%s/%s")'%(srm_burt, loc%'Mu'),       lumi = 9999 ) #4428.4134 )
+lepton118.add("SingleMu.2012B", '%s/%s", alwaysUseLastAttempt = True)'%(srm_burt, loc%'Mu'),       lumi = 9999 ) #4428.4134 )
 
 loc = "2013_01_15_22_31_59/Single%s.Run2012C-TOP%sPlusJets-19Dec2012-v1.AOD"
 lepton118.add("SingleEl.2012C", '%s/%s")'%(srm_burt, loc%('Electron','Ele')), lumi = 9999 ) #461.7498 )
