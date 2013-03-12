8ba0005f86e1bf0330ae338be76409af3a1a44b6
Burton Betchart <betchart@gmail.com>
2013-03-12 19:09:52 +0000

    new options for batch.py

--- batch.py
+++ batch.py
@@ -5,9 +5,10 @@ parser = OptionParser("usage: %prog tagSuffix [options]")
 parser.add_option("--all",   dest = "doAll",     default = False,  action='store_true',  help = "submit all samples")
 parser.add_option("--tops",  dest = "justTops",  default = False,  action='store_true',  help = "submit ttbar samples")
 parser.add_option("--wjets", dest = "justWJets", default = False,  action='store_true',  help = "submit wjets samples")
+parser.add_option("--nodata",dest = "noData",    default = False,  action='store_true',  help = 'submit all samples except data')
 
 options,args = parser.parse_args()
-if len(args)!=1 or sum(int(i) for i in [options.doAll,options.justTops,options.justWJets])!=1:
+if len(args)!=1 or sum(int(i) for i in [options.doAll,options.justTops,options.justWJets,options.noData])!=1:
     parser.print_help()
     exit()
 
@@ -37,13 +38,17 @@ groupsTop = {140:['ttj_%s.wGG.pu'%top,],
              }
 
 groupsW = {80:['w2j_mg.pu'],
-          40:['w%dj_mg.py'%d for d in [1,3,4]]
+          40:['w%dj_mg.pu'%d for d in [1,3,4]]
           }
 
+
+groupsNoData = {20:['.'.join([lep,part,'jw']) for part in ['A.1','A.2','B.1','C.1','C.2','C.3','D.1'] for lep in ['Mu','El']]}
+
 cmds = []
-groups = groupsW if options.justWJets else groupsTop if options.justTops else groupsAll
+groups = groupsW if options.justWJets else groupsTop if options.justTops else groupsNoData if options.noData else groupsAll
+samplesOption = '--omit' if options.noData else '--samples'
 for nslices,samples in groups.items() :
-    cmds.append('supy analyses/topAsymm.py --tags %s --samples %s --loop 2 --slices %d --batch'%(tags,','.join(samples),nslices))
+    cmds.append('supy analyses/topAsymm.py --tags %s %s %s --loop 2 --slices %d --batch'%(tags,samplesOption,','.join(samples),nslices))
 if options.doAll : cmds.append('supy analyses/topAsymm.py --tags %s --omit %s --loop 2 --slices %d --batch'%(tags,','.join(sum([samples for samples in groups.values()],[])),40))
 
 for cmd in cmds : print cmd
