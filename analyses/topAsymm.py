59ef38f51cbfec235e8a0c3f9e1bcf2daac186b1
Burton Betchart <betchart@gmail.com>
2013-08-07 18:55:31 -0500

    turns out we need 3D

--- analyses/topAsymm.py
+++ analyses/topAsymm.py
@@ -311,7 +311,7 @@ class topAsymm(supy.analysis) :
         triD = ('TridiscriminantWTopQCD',5,-1,1)
         isData = pars['sample'].split('.')[0] in ['El','Mu']
 
-        asymm = "steps.top.signalhists(doGen=False)"
+        asymm = "steps.top.signalhists(doGen=not saDisable)"
         kinem = "steps.top.kinematics3D('fitTop')"
         doSys = self.doSystematics(pars)
 
