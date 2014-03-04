36589a172be78ef233980fce99b54424fec851d9
Burton Betchart <betchart@gmail.com>
2014-03-04 13:52:16 -0600

    axigluon calibration samples

--- samples/__top119_fnal__.py
+++ samples/__top119_fnal__.py
@@ -22,4 +22,19 @@ top119_fnal.add("calib_mn", pnfs_bb + '/TOP/automated/2013_11_25_22_52_14/")', x
 ttj_mg = '/TOP/automated/2013_11_25_23_05_40/TTJets_SemiLeptMGDecays_8TeV-madgraph-tauola.Summer12_DR53X-PU_S10_START53_V19_ext%d-v1.AODSIM'
 top119_fnal.add("calib_mg", '+'.join(['%s/%s")'%(pnfs_bb,ttj_mg%v) for v in [1,2]]), xs = 53.2)
 
+axigluons = "/TOP/automated/2014_01_30_00_49_16/"
+
+L200 = "HeavyGluonToTT_left_M-200_TuneZ2star_8TeV-madgraph-tauola.Summer12_DR53X-PU_S10_START53_V19-v1.AODSIM"
+A200 = "HeavyGluonToTT_axial_M-200_TuneZ2star_8TeV-madgraph-tauola.Summer12_DR53X-PU_S10_START53_V19-v1.AODSIM"
+R200 = "HeavyGluonToTT_right_M-200_TuneZ2star_8TeV-madgraph-tauola.Summer12_DR53X-PU_S10_START53_V19-v1.AODSIM"
+A2K = "HeavyGluonToTT_axial_M-2000_TuneZ2star_8TeV-madgraph-tauola.Summer12_DR53X-PU_S10_START53_V19-v1.AODSIM"
+R2K = "HeavyGluonToTT_right_M-2000_TuneZ2star_8TeV-madgraph-tauola.Summer12_DR53X-PU_S10_START53_V19-v1.AODSIM"
+ZP = "ZprimeTtoTTU_M-220_TuneZ2star_8TeV-madgraph-tauola.Summer12_DR53X-PU_S10_START53_V19-v1.AODSIM"
+
+top119_fnal.add("calib_L200", pnfs_bb + axigluons + L200 + '")', xs = 211)
+top119_fnal.add("calib_A200", pnfs_bb + axigluons + A200 + '")', xs = 211)
+top119_fnal.add("calib_R200", pnfs_bb + axigluons + R200 + '")', xs = 211)
+top119_fnal.add("calib_A2K", pnfs_bb + axigluons + A2K + '")', xs = 211)
+top119_fnal.add("calib_R2K", pnfs_bb + axigluons + R2K + '")', xs = 211)
+top119_fnal.add("calib_ZP", pnfs_bb + axigluons + ZP + '")', xs = 211)
 
