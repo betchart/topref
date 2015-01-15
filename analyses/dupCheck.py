81b2164ae8ceab634ab738a487c83495be2c93b3
Burton Betchart <betchart@gmail.com>
2015-01-15 14:06:52 -0600

    revise dupCheck to run over topAsymm data samples; new supy tag with pruning of CRAB duplicates for EOS and Disk listings

--- analyses/dupCheck.py
+++ analyses/dupCheck.py
@@ -1,17 +1,24 @@
 import supy,steps,samples,calculables
 
 class dupCheck(supy.analysis) :
-    def parameters(self) :
-        group = self.vary()
-        group['SingleMu'] = ["MuHad.2012A_1",
-                             "MuHad.2012A_2",
-                             "SingleMu.2012B",
-                             "SingleMu.2012C"]
-        group['SingleEl'] = ["ElHad.2012A_1",
-                             "ElHad.2012A_2",
-                             "SingleEl.2012B",
-                             "SingleEl.2012C"]
-        return {'group':group}
+
+    def parameters(self):
+        return {'lepton':self.vary({'el':{'name':'el'},'mu':{'name':'mu'}})}
+
+    @staticmethod
+    def muons(suffix="") :     return [f+suffix for f in ['Mu.A.1','Mu.A.2','Mu.B.1','Mu.C.1','Mu.C.2','Mu.C.3','Mu.D.1']]
+    @staticmethod
+    def electrons(suffix="") : return [f+suffix for f in ['El.A.1','El.A.2','El.B.1','El.C.1','El.C.2','El.C.3','El.D.1']]
+    @staticmethod
+    def jsonFiles() :
+        return ['lumi/json/'+f for f in ['Cert_190456-196531_8TeV_13Jul2012ReReco_Collisions12_JSON.txt',
+                                         'Cert_190782-190949_8TeV_06Aug2012ReReco_Collisions12_JSON.txt',
+                                         'Cert_190456-196531_8TeV_13Jul2012ReReco_Collisions12_JSON.txt',
+                                         'Cert_198022-198523_8TeV_24Aug2012ReReco_Collisions12_JSON.txt',
+                                         'Cert_190456-208686_8TeV_PromptReco-NoReprocessing_Collisions12CD_JSON.txt',
+                                         'Cert_201191-201191_8TeV_11Dec2012ReReco-recover_Collisions12_JSON.txt',
+                                         'Cert_190456-208686_8TeV_PromptReco-NoReprocessing_Collisions12CD_JSON.txt']]
+
     
     def listOfSteps(self,pars) :
         return [supy.steps.printer.progressPrinter(2,300),
@@ -21,7 +28,16 @@ class dupCheck(supy.analysis) :
         return supy.calculables.zeroArgs(supy.calculables)
 
     def listOfSamples(self,pars) :
-        return sum([supy.samples.specify(names = samps) for samps in pars['group']],[])
+        return sum( [supy.samples.specify( names = ds, weights = calculables.other.jw(jfn))
+                     for ds,jfn in zip({"mu":self.muons(),
+                                        "el":self.electrons()}[pars['lepton']['name']],
+                                       self.jsonFiles())],[])
 
     def listOfSampleDictionaries(self) :
-        return [samples.lepton112]
+        return [samples.lepton119_fnal]
+
+
+    def conclude(self,pars):
+        org = self.organizer(pars, verbose = True )
+        supy.plotter(org, pdfFileName = self.pdfFileName(org.tag), doLog=False ).plotAll()
+        
